298958c76d8bddd9461f4c55136f7e23726ceab1
Nick Brunal <nickbrunal@gmail.com>
2019-08-08 00:45:40 -0400

    put together fectchPostWithQuery action to recieve queried posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import { SET_RECENT_POSTS } from "./types";
 import axios from "axios";
 
 export function fetchRecentPosts() {
-  return function(dispatch) {
+  return function (dispatch) {
     axios.get("https://api.dailysmarty.com/posts").then(response => {
       console.log(response.data.posts);
       dispatch({
@@ -11,5 +11,18 @@ export function fetchRecentPosts() {
         payload: response.data.posts
       });
     });
+  };
+}
+
+export function fetchPostsWithQuery(query) {
+  return function (dispatch) {
+    axios.get(`https://api.dailysmarty.com/search?q=${query}`)
+      .then(response => {
+        console.log(response.data.posts);
+        // dispatch({
+        //   type: SET_RECENT_POSTS,
+        //   payload: response.data.posts
+        // });
+      });
   };
 }
